62bdab12dfbdeaa85613bc7c0fb98699d917529e
Haroon Javed <mharoon.j@hotmail.com>
2023-07-05 13:09:28 +0200

    adding a loader

--- src/components/movies/index.tsx
+++ src/components/movies/index.tsx
@@ -1,15 +1,25 @@
 import React from "react";
-import Card from "./Card";
-import MoviesList from "./MoviesList";
-import SearchBar from "./SearchBar";
+import MoviesList from "./lists/MoviesList";
+import SearchBar from "../searchbars/SearchBar";
+import ClipLoader from "react-spinners/ClipLoader";
 
 type MovieSearchProps = {};
 
 const MoviesSearch: React.FC<MovieSearchProps> = () => {
-  return <div>
-    <SearchBar />
-    <MoviesList />
-  </div>;
+  return (
+    <div style={{ width: "100%" }}>
+      <SearchBar />
+      <MoviesList />
+      <ClipLoader
+        color={"#ffffff"}
+        loading={false}
+        // cssOverride={override}
+        size={150}
+        aria-label="Loading Spinner"
+        data-testid="loader"
+      />
+    </div>
+  );
 };
 
 export default MoviesSearch;
