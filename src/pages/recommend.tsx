bfec7d5cf9ef9a2bec1b986b759c338ca4f6882c
Haroon Javed <mharoon.j@hotmail.com>
2023-07-11 21:37:40 +0200

    implementing tmdb api for popular movies and making some changes in styles

--- src/pages/recommend.tsx
+++ src/pages/recommend.tsx
@@ -1,10 +1,19 @@
-import Head from 'next/head'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Recommend.module.css'
+import Head from "next/head";
+import { Inter } from "next/font/google";
+import styles from "@/styles/Recommend.module.css";
+import MoviesList from "../components/movies/lists/MoviesList";
+import config from "../../config.json";
+import { MovieCardInterface } from "@/components/movies/types/movie-types";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
-export default function Recommend() {
+interface MovieRecommendationsProps {
+  recommendedMovies: MovieCardInterface[];
+}
+
+const Recommend: React.FC<MovieRecommendationsProps> = ({
+  recommendedMovies,
+}) => {
   return (
     <>
       <Head>
@@ -15,8 +24,35 @@ export default function Recommend() {
       </Head>
       <main className={`${styles.main} ${inter.className}`}>
         {/* update with recommendations page code */}
-        Edit file [src/pages/recommend.tsx] to update this page with recommendations logic.
+        <MoviesList movies={recommendedMovies} nextPage={() => {}} />
       </main>
     </>
-  )
-}
+  );
+};
+
+export const getStaticProps = async () => {
+  const recommendations = await fetcher(
+    `${config.TMDB_SERVER_URI}/3/movie/popular?language=en-US&page=1&api_key=${config.TMDB_API_KEY}`
+  );
+  const { results } = recommendations;
+  let recommendedMovies: MovieCardInterface[] = [];
+  for (const movie of results.slice(0, 10)) {
+    const result = await fetcher(
+      `${config.SERVER_URI}/?apikey=${config.API_KEY}&s=${movie.title}&page=1`
+    );
+
+    if (result.Search) {
+      recommendedMovies.push(result.Search[0]);
+    }
+  }
+
+  return {
+    props: {
+      recommendedMovies,
+    },
+    revalidate: 86400, // Refresh every 24 hours (in seconds)
+  };
+};
+const fetcher = (url: string) => fetch(url).then((res) => res.json());
+
+export default Recommend;
